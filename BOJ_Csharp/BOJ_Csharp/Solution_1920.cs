887fe0a93862052da5eab610adb540a1a431b413
Andy Ahn <blackhan25@daum.net>
2020-08-06 17:21:42 +0900

    added sol for 1920

---
+++ BOJ_Csharp/BOJ_Csharp/Solution_1920.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+public class Program_1920
+{
+	static void Main()
+	{
+        Solution_1920();
+	}
+
+    public static void Solution_1920()
+    {
+        var N = int.Parse(Console.ReadLine());
+        var listed = Console.ReadLine().Split().Select(x => int.Parse(x));
+        var M = int.Parse(Console.ReadLine());
+        var candidiates = Console.ReadLine().Split().Select(x => int.Parse(x));
+        if (listed.Any(M))
+        {
+            Console.WriteLine("1");
+        }
+        else
+        {
+            Console.WriteLine("0");
+        }
+    }
+}
